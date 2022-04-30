425dbf8464f697d4f42d71d2968526b78ece1277
Corey <1609495628@qq.com>
2022-05-01 07:43:18 +0800

    c

---
+++ src/main/java/com/example/javademo/m5_jvm/p_1_classLoader/init/T.java
@@ -0,0 +1,18 @@
+package com.example.javademo.m5_jvm.p_1_classLoader.init;
+
+/**
+ * 2022/4/17
+ */
+public class T {
+
+    public static void main(String[] args) {
+        if (true){
+            System.out.println(1);
+        }else {
+            System.out.println(2);
+        }
+    }
+
+
+}
+
